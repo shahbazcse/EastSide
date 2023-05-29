bfe9dda3b4b61bd0d451de701e41d9d56c08d6a7
shahbazcse <cseshahbaz@gmail.com>
2023-05-29 18:16:46 +0530

    Completed Cart Management

--- eastside/src/pages/Cart.jsx
+++ eastside/src/pages/Cart.jsx
@@ -1,18 +1,52 @@
 import { useContext, useEffect, useState } from "react";
-import ProductCard from "../components/ProductCard";
+import CartCard from "../components/CartCard";
 import { AppContext } from "../contexts/AppContext";
 
 export default function Cart() {
   const { cart } = useContext(AppContext);
 
+  const totalPrice = cart.reduce(
+    (acc, { price, units }) => acc + price * units,
+    0
+  );
+
+  const itemCount = cart.length > 0;
+
   return (
     <div>
       <h1>Cart</h1>
-      <div className="list">
+      <div>
         {cart.map((prod) => (
-          <ProductCard {...prod} key={prod.id} />
+          <CartCard {...prod} key={prod.id} />
         ))}
       </div>
+      {itemCount && (
+        <div className="card">
+          <h3>CART PRICE DETAILS</h3>
+          <hr />
+          {cart.map(({ name, units, price }) => (
+            <p>
+              <span>
+                {name} ({units}) Rs.{price}
+              </span>
+            </p>
+          ))}
+          <hr />
+          <p>
+            <strong>Total Price:</strong> Rs.{totalPrice}
+          </p>
+          <button
+            style={{
+              borderRadius: "16px",
+              padding: "0.5rem",
+              backgroundColor: "blue",
+              color: "white",
+            }}
+          >
+            CHECKOUT
+          </button>
+        </div>
+      )}
     </div>
   );
 }
