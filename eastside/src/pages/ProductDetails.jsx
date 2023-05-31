e7c16c5798e62b40a052c0506423c38b459b7967
shahbazcse <cseshahbaz@gmail.com>
2023-05-31 22:58:01 +0530

    Refactored Product Details Page

--- eastside/src/pages/ProductDetails.jsx
+++ eastside/src/pages/ProductDetails.jsx
@@ -1,100 +1,20 @@
 import { useContext } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { AppContext } from "../contexts/AppContext";
+import ProductCard from "../components/ProductCard";
 
 export default function ProductDetails() {
   const { productId } = useParams();
 
-  const { state, dispatch } = useContext(AppContext);
-  const { data } = state;
+  const { state } = useContext(AppContext);
 
-  const prod = data.find(({ id }) => id === Number(productId));
+  const allProducts = [...state.products];
 
-  const {
-    id,
-    name,
-    description,
-    price,
-    quantity,
-    category,
-    brand,
-    units,
-    inWishlist,
-    inCart
-  } = prod;
-
-  const cartPage = window.location.pathname === "/cart";
+  const prod = allProducts.find(({ id }) => id === Number(productId));
 
   return (
-    <div key={id} className="card">
-      <Link to={`/products/product/${id}`}>
-        <strong>
-          <p>{name}</p>
-        </strong>
-      </Link>
-      <p>
-        <strong>Brand:</strong> {brand}
-      </p>
-      <p>
-        <strong>About:</strong> {description}
-      </p>
-      <p>
-        <strong>Price:</strong> Rs.{price}
-      </p>
-      <p>
-        <strong>Available In Stock:</strong> {quantity}
-      </p>
-      <p>
-        <strong>Category:</strong> {category}
-      </p>
-      {cartPage && (
-        <span>
-          <button
-            onClick={() => dispatch({ type: "decreaseQuantity", payload: id })}
-          >
-            -
-          </button>
-          <span> {units} </span>
-          <button
-            onClick={() => dispatch({ type: "increaseQuantity", payload: id })}
-          >
-            +
-          </button>
-        </span>
-      )}
-      <button
-        onClick={() =>
-          !inWishlist
-            ? dispatch({ type: "addToWishlist", payload: prod })
-            : dispatch({ type: "removeFromWishlist", payload: prod })
-        }
-      >
-        {!inWishlist ? "Add to Wishlist" : "Remove from wishlist"}
-      </button>
-      {!cartPage ? (
-        !inCart ? (
-          <button
-            onClick={() => dispatch({ type: "addToCart", payload: prod })}
-          >
-            Add To Cart
-          </button>
-        ) : (
-          <button>
-            <Link
-              to="/cart"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              Go to Cart
-            </Link>
-          </button>
-        )
-      ) : (
-        <button
-          onClick={() => dispatch({ type: "removeFromCart", payload: prod })}
-        >
-          Remove From Cart
-        </button>
-      )}
+    <div className="list" style={{ alignSelf: "center" }}>
+      <ProductCard {...prod} />
     </div>
   );
 }
