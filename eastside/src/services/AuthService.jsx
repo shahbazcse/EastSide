e1cbf094d0cca0ffaccf9d502aee252a3f8a2017
shahbazcse <cseshahbaz@gmail.com>
2023-07-05 23:57:49 +0530

    Storing session in localStorage

--- eastside/src/services/AuthService.jsx
+++ eastside/src/services/AuthService.jsx
@@ -2,10 +2,24 @@ import axios from "axios";
 
 export const loginUser = async (formData) => {
   const response = await axios.post(`/api/auth/login`, formData);
+  localStorage.setItem(
+    "session",
+    JSON.stringify({
+      user: response.data.foundUser,
+      encodedToken: response.data.encodedToken,
+    })
+  );
   return response.data;
 };
 
 export const signupUser = async (formData) => {
   const response = await axios.post("/api/auth/signup", formData);
+  localStorage.setItem(
+    "session",
+    JSON.stringify({
+      user: response.data.foundUser,
+      encodedToken: response.data.encodedToken,
+    })
+  );
   return response.data;
 };
